dc11bd6483e6225498ab3e04fb733bb35ed040cc
darcyiiix <chillfrags98@gmail.com>
2024-06-18 20:04:12 +0500

    removed code

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use('/api/images', imageRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
-}
+});
 
 app.use('/api/config/paypal', (req, res) => {
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
