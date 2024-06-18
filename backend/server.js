5049894ca939fdd4a141c91cf03aecb31b91e811
darcyiiix <chillfrags98@gmail.com>
2024-06-18 20:29:35 +0500

    uploading to production

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -37,33 +35,18 @@ app.use('/api/config/paypal', (req, res) => {
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 });
 
-// router.put('/products/:productId/decrement', async (req, res) => {
-//     const productId = req.params.productId;
-//     const { qty } = req.body;
-  
-//     try {
-//       // Find the product by its ID
-//       const product = await Product.findById(productId);
-  
-//       if (!product) {
-//         return res.status(404).json({ message: 'Product not found' });
-//       }
-  
-//       // Decrement the product quantity
-//       product.countInStock -= qty;
-  
-//       // Save the updated product
-//       await product.save();
-  
-//       res.json({ message: 'Product quantity decremented successfully', product });
-//     } catch (error) {
-//       console.error('Error decrementing product quantity:', error);
-//       res.status(500).json({ message: 'Internal server error' });
-//     }
-//   });
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+}
 
 app.use(notFound);
 app.use(errorHandler);
